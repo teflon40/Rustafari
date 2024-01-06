8e29911c2227aebfb188d989eff3669568808b8c
teflon40 <frimpong.bernard3220@gmail.com>
2024-01-06 13:24:40 +0100

    Hello cargo

---
+++ hello_cargo/target/debug/deps/hello_cargo-4376c0aedfff852b.d
@@ -0,0 +1,5 @@
+/home/teflon40/me/Rustafari/hello_cargo/target/debug/deps/hello_cargo-4376c0aedfff852b: src/main.rs
+
+/home/teflon40/me/Rustafari/hello_cargo/target/debug/deps/hello_cargo-4376c0aedfff852b.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/teflon40/me/Rustafari/hello_cargo/target/debug/hello_cargo: /home/teflon40/me/Rustafari/hello_cargo/src/main.rs
